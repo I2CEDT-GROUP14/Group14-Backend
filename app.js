a7235479505d5b405a68e98fc9c478834c0ece58
Rachatat Bunpat <82020386+rbunpat@users.noreply.github.com>
2025-09-09 21:14:33 +0700

    asd

--- app.js
+++ app.js
@@ -10,6 +10,11 @@ app.use(express.urlencoded({ extended: true }));
 
 app.use(cors());
 
+app.use("/" , (req, res) => {
+    res.sendFile('static/bonk.jpg', { root: process.cwd() }, (err) => {
+        if (err) res.status(err.status || 500).end();
+    });
+});
 app.use("/status", statusRoute);
 
 export default app;
