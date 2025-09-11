ef59bc4ae7b0eaed9f27006e6589088cd185fc51
Rachatat Bunpat <82020386+rbunpat@users.noreply.github.com>
2025-09-11 14:52:40 +0700

    bruh

--- controllers/statusController.js
+++ controllers/statusController.js
@@ -1,5 +1,5 @@
 export const getStatus = async (req, res) => {
-  res.status(200).json({ status: "OK", message: "Service is running" });
+  res.status(200).json({ status: "OK", message: "Backend service is running" });
 };
 
 export default { getStatus };
