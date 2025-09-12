46a0d7d9e74e139893b012b8fc2f7dc83c068607
Rachatat Bunpat <82020386+rbunpat@users.noreply.github.com>
2025-09-12 15:49:55 +0700

    Remove commented-out code and clean up app.js, db.js, and generateQuiz.js for better readability

--- app.js
+++ app.js
@@ -19,16 +19,6 @@ app.use("/quiz", quizRoute);
 app.use("/tag", tagRoute);
 
 app.use("/" , (req, res) => {
-    //randomly send file bonk.jpg from static folder
-    // const fileName = Math.random() < 0.5 ? '1.jpg' : '2.jpg';
-    // res.sendFile(`static/${fileName}`, { root: process.cwd() }, (err) => {
-    //     if (err) res.status(err.status || 500).end();
-    // });
-    // res.send("Welcome to Quiz API");
-    // res.sendFile('static/1.jpg', { root: process.cwd() }, (err) => {
-    //     if (err) res.status(err.status || 500).end();
-    // });
-    //send base64 encoded image
     res.send("(╯‵□′)╯︵┻━┻");
 });
 
--- db/db.js
+++ db/db.js
@@ -2,6 +2,5 @@ import mongoose from "mongoose";
 import dotenv from "dotenv";
 
 dotenv.config();
-// console.log(process.env.MONGODB_URI)
 
 mongoose.connect(process.env.MONGODB_URI);
